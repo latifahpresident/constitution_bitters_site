91ce8d809e507e98100c288b1a4006d389efe839
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-08-01 11:16:30 -0500

    added assets and homepage component

---
+++ bitter_frontend/src/global-styles/colors.js
@@ -0,0 +1,20 @@
+const accentColors = [
+    '#FE9503'
+]
+
+const greenColors = [
+    '#384611',
+    '#73FF60'
+]
+
+const fontColors = [
+    '#535351',
+    '#1F2020',
+    '#A1A2A4'
+]
+
+export {
+    accentColors,
+    greenColors,
+    fontColors
+}
