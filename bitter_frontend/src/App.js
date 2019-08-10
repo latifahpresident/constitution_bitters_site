5ea0c9d6b514651b04e0d3bd2b4be062564d4dc9
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-08-09 23:10:47 -0500

    header setup

--- bitter_frontend/src/App.js
+++ bitter_frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Nav from './components/nav/Nav';
+import Header from './components/header/Header';
 import LandingPage from './views/landing-page/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Header/>
       <Route exact path = '/' render={(props) => <LandingPage {...props}/>} />
     </div>
   );
---
+++ bitter_frontend/src/components/header/header-styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const SocialBanner = styled.div`
+    border: 1px solid red;
+`;
